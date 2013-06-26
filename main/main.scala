86bda4b1d175eb9895114a760471f478a3d3c71e
Jason Zaugg <jzaugg@gmail.com>
2013-06-26 17:09:47 +0200

    An existential test.
    But it doesn't motivate any use of unpack-/skolemize-Existential, as I had hoped.

--- main/main.scala
+++ main/main.scala
@@ -6,6 +6,10 @@ object MacroTest {
       def foo(p: y.type): y.type = y
       def bar[A] = ()
       bar[y.type]
+
+      var seq: Seq[_ <: x.type] = Nil
+      seq = seq
+
       val z: y.type = foo({"boo"; x})
       (() => {
         println("called");
